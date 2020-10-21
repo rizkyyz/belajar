a16275039e6a13c002143b3903e67667b77c0895
rizkyyz <66506609+rizkyyz@users.noreply.github.com>
2020-10-22 00:07:07 +0700

    Update biodata.py

--- biodata.py
+++ biodata.py
@@ -10,7 +10,7 @@ agama= raw_input ("Masukan Agama: ")
 pekerjaan= raw_input ("Pekerjaan: ")
 hoby= raw_input ("Masukan Hoby: ")
 km= raw_input ("Kata Mutiara: ")
-kt= raw_input ("Kata kata: ")
+kt= raw_input ("Kata Kata: ")
 
 #Menampilkan Inputan User
 print "============================"
