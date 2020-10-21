0e1514c9373bbd5b4eea1c96eeae7dcae77a5382
rizkyyz <66506609+rizkyyz@users.noreply.github.com>
2020-10-21 23:43:05 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,25 @@
+print "============="
+print "   Biodata   "
+print "============="
+#variabel
+nama= raw_input ("Masukan Nama Anda: ")
+alamat= raw_input ("Masukan Alamat: ")
+ttl= raw_input ("Masukan Tempat Tanggal Lahir Anda: ")
+jk= raw_input ("Jenis Kelamin: ")
+agama= raw_input ("Masukan Agama: ")
+pekerjaan= raw_input ("Pekerjaan: ")
+hoby= raw_input ("Masukan Hoby: ")
+km= raw_input ("Kata Mutiara: ")
+
+#Menampilkan Inputan User
+print "============"
+print "Biodata Anda"
+print "============"
+print "Nama: ",nama
+print "Alamat: ",alamat
+print "Tempat Tanggal Lahir: ",ttl
+print "Jenis Kelamin: ",jk
+print "Agama: ",agama
+print "Pekerjaan: ",pekerjaan
+print "Hoby: ",hoby
+print "Kata Mutiara: ",km
