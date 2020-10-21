078c8d17531bfe45ba4b927fbc5a0a68958a8461
rizkyyz <66506609+rizkyyz@users.noreply.github.com>
2020-10-22 00:04:35 +0700

    Update biodata.py

--- biodata.py
+++ biodata.py
@@ -10,18 +10,19 @@ agama= raw_input ("Masukan Agama: ")
 pekerjaan= raw_input ("Pekerjaan: ")
 hoby= raw_input ("Masukan Hoby: ")
 km= raw_input ("Kata Mutiara: ")
+kt= raw_input ("Kata kata: ")
 
 #Menampilkan Inputan User
 print "============================"
 print "       Biodata Anda         "
 print "============================"
-print "Nama                : ",nama
-print "Alamat              : ",alamat
-print "Tempat Tanggal Lahir: ",ttl
-print "Jenis Kelamin       : ",jk
-print "Agama               : ",agama
-print "Pekerjaan           : ",pekerjaan
-print "Hoby                : ",hoby
-print "Kata Mutiara        : ",km
-
+print "     Nama           : ",nama
+print "     Alamat         : ",alamat
+print "     Tempat Tanggal Lahir: ",ttl
+print "     Jenis Kelamin  : ",jk
+print "     Agama          : ",agama
+print "     Pekerjaan      : ",pekerjaan
+print "     Hoby           : ",hoby
+print "     Kata Mutiara   : ",km
+Print "     Kata Kata      : ",kt
 
