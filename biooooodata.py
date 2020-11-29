3aa63fd1f98dee2547cbafec5265e8b06121877d
rizkyyz <66506609+rizkyyz@users.noreply.github.com>
2020-11-30 00:11:38 +0700

    Update biooooodata.py

--- biooooodata.py
+++ biooooodata.py
@@ -1,15 +1,44 @@
-print "  ==================================  "
-print "           Rizkyyzz Ganteng          "
-print "  ==================================  "
-#variabel
-nama= raw_input ("Masukan Nama Lengkap Anda: ")
-panggilan= raw_input ("Masukan Nama Panggilan: ")
-nim= raw_input ("Masukan Nim Anda: ")
-ttl= raw_input ("Masukan Tempat Lahir Anda: ")
-tl= input ("Masukan Tanggal lahir  Anda: ")
-telp= raw_input ("Masukan No Telpon Anda: ")
-alamat= raw_input ("Masukan Alamat Anda: ")
+nim = []
+nama = []
+asal = []
 
+pilihan = 1
+while pilihan != 0 :
+    print ("1. masukan data.")
+    print ("2. tampilkan data.")
+    print ("3. hapus data.")
+    print ("0. exit.")
 
-#Menampilkan Inputan User
-print "Let me introduce my self My name is",nama,"but you can call me",panggilan,".My NIM is",nim,"I was born in",ttl,"and I am",tl,"years old. I am very glad if you want to invite my house in",alamat,".So, don't forget to call me before with the number",telp,
+    pilihan = int(input("masukan pilihan anda : "))
+    print('')
+    print('')
+    print('')
+    if pilihan == 1 :
+        masnim = input("masukan nim : ")
+        nim.append({'nim' : masnim})
+        masnama = input("masukan nama : ")
+        nama.append({'nama' : masnama})
+        masasal = input("masukan asal : ")
+        asal.append({'asal' : masasal})
+        
+        
+    elif pilihan == 2 :
+        penentu = True
+        for i in range (len(nim)) :
+            if penentu :
+                print ("nim\tnama\tasal")
+            print (nim[i]['nim'],'\t',nama[i]['nama'],'\t',asal[i]['asal'])
+            penentu = False
+            
+    elif pilihan == 3 :
+        masnim = input("masukan nim : ")
+        for i in range (len(nim)) :
+            if masnim == nim[i]['nim'] :
+                print (i)
+                del nim[i]
+                del nama[i]
+                del asal[i]
+                break
+    print('')
+    print('')
+    print('')
